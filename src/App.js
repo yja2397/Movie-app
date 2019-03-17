c83a49484583e004c87f9148c705ebd689ac7acb
Seongtae-Kim <chris.seongtae@gmail.com>
2019-03-17 18:04:13 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        영화 챗봇!<br/><br/>
+        제작자 : <br/>
+        201504113 영어영문학과 김성태<br/>
+        201601157 정보통계학과 윤지애 
+      </div>
+    );
+  }
+}
+
+export default App;
