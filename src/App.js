07096e458d39c747bb145e1d457be3a4111612c9
Seongtae-Kim <chris.seongtae@gmail.com>
2019-03-21 01:32:34 +0900

    first revision

--- src/App.js
+++ src/App.js
@@ -1,17 +1,98 @@
-import React, { Component } from 'react';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        영화 챗봇!<br/><br/>
-        제작자 : <br/>
-        201504113 영어영문학과 김성태<br/>
-        201601157 정보통계학과 윤지애 
-      </div>
-    );
-  }
-}
 
-export default App;
+ import React, { Component } from 'react';
+ import Pusher from 'pusher-js';
+ import './App.css';
+
+ class App extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       userMessage: '',
+       conversation: [],
+     };
+   }
+
+   componentDidMount() {
+     const pusher = new Pusher('<your app key>', {
+       cluster: '<your app cluster>',
+       encrypted: true,
+     });
+
+     const channel = pusher.subscribe('bot');
+     channel.bind('bot-response', data => {
+       const msg = {
+         text: data.message,
+         user: 'ai',
+       };
+       this.setState({
+         conversation: [...this.state.conversation, msg],
+       });
+     });
+   }
+
+   handleChange = event => {
+     this.setState({ userMessage: event.target.value });
+   };
+
+   handleSubmit = event => {
+     event.preventDefault();
+     if (!this.state.userMessage.trim()) return;
+
+     const msg = {
+       text: this.state.userMessage,
+       user: 'human',
+     };
+
+     this.setState({
+       conversation: [...this.state.conversation, msg],
+     });
+
+     fetch('http://localhost:5000/chat', {
+       method: 'POST',
+       headers: { 'Content-Type': 'application/json' },
+       body: JSON.stringify({
+         message: this.state.userMessage,
+       }),
+     });
+
+     this.setState({ userMessage: '' });
+   };
+
+   render() {
+     const ChatBubble = (text, i, className) => {
+       return (
+         <div key={`${className}-${i}`} className={`${className} chat-bubble`}>
+           <span className="chat-content">{text}</span>
+         </div>
+       );
+     };
+
+     const chat = this.state.conversation.map((e, index) =>
+       ChatBubble(e.text, index, e.user)
+     );
+
+     return (
+       <div>
+         <h1>영화 추천 챗봇!</h1>
+         <div className="chat-window">
+           <div className="conversation-view">{chat}</div>
+           <div className="message-box">
+             <form onSubmit={this.handleSubmit}>
+               <input
+                 value={this.state.userMessage}
+                 onInput={this.handleChange}
+                 className="text-input"
+                 type="text"
+                 autoFocus
+                 placeholder="어떤 영화든 물어만 보세요!"
+               />
+             </form>
+           </div>
+         </div>
+       </div>
+     );
+   }
+ }
+
+ export default App;
---
+++ src/server.js
@@ -0,0 +1,24 @@
+// server.js
+
+require('dotenv').config({ path: 'variables.env' });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/chat', (req, res) => {
+  const { message } = req.body;
+  console.log(message);
+});
+
+app.set('port', process.env.PORT || 5000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
